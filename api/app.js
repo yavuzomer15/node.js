83eb815a360fe3417a818999fd0bc3aaf9af7d2d
yavuzomer15 <yavuzomer2003@outlook.com>
2025-07-10 01:55:02 +0300

    second commit

--- api/app.js
+++ api/app.js
@@ -1,3 +1,8 @@
+
+if (process.env.NODE_ENV != "production")
+  require('dotenv').config()
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -23,12 +28,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
