c6372817911289c2d3a166e78acf70b70243e097
yavuzomer15 <yavuzomer2003@outlook.com>
2025-07-11 01:50:55 +0300

    3

--- api/config/index.js
+++ api/config/index.js
@@ -1,4 +1,5 @@
+require('dotenv').config();
 module.exports = {
     "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
-    "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://localhost:27017/node.js"
+    "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://127.0.0.1:27017/nodejs"
 }
---
+++ api/db/Database.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+let instance = null;
+
+class Database {
+
+    constructor() {
+        if (!instance) {
+            instance = this;
+        }
+        return instance;
+    }
+
+    async connect(options) {
+        try {
+            console.log("DB Connecting...");
+            let db = await mongoose.connect(options.CONNECTION_STRING);
+
+            this.mongoConnection = db;
+            console.log("DB Connected.");
+
+        } catch (err) {
+            console.error(err);
+            process.exit(1);
+        }
+
+    }
+
+}
+
+module.exports = Database;
